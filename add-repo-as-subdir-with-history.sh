baceeeb65a45f0a0b75a58177acf2a576c9b3671
Argens <ng.argens@gmail.com>
2023-06-01 21:13:36 +0800

    fix: typo

--- add-repo-as-subdir-with-history.sh
+++ add-repo-as-subdir-with-history.sh
@@ -1,10 +1,13 @@
-set TREE_NAME=any-string
-set GIT_PATH=git@githost.com:user/repo.git
-set BRANCH=main
-set COMMIT_MESSAGE='Merge any-string as our subdirectory'
+TREE=$1
+DIR=$TREE
+GIT_PATH=git@github.com:argens1203/$2.git
+BRANCH="${3:-main}"
+COMMIT_MESSAGE="Merge $1 as our subdirectory"
+
+echo "git merge -s ours --no-commit --allow-unrelated-histories $TREE/$BRANCH"
 
 git remote add -f $TREE $GIT_PATH
-get merge -s ours --no-commit --allow-unrelated-histores $TREE/$BRANCH
+git merge -s ours --no-commit --allow-unrelated-histories $TREE/$BRANCH
 git read-tree --prefix=$DIR -u $TREE/$BRANCH
 git commit -m "$COMMIT_MESSAGE"
 git pull -s subtree $TREE $BRANCH
